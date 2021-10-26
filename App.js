fadbf4e43bdcfb0fe3b4184d16aaf00207a7dc16
sirajul Mostafa <sirajulmost@gmail.com>
2021-10-26 21:08:41 +0600

    this is part for login page part1

---
+++ App.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import {StyleSheet} from 'react-native';
+
+import LoginScreen from './//src/Screens/Login';
+
+const App = () => {
+  return <LoginScreen />;
+};
+
+const styles = StyleSheet.create({
+  // sectionContainer: {
+  //   marginTop: 32,
+  //   paddingHorizontal: 24,
+  // },
+  // sectionTitle: {
+  //   fontSize: 24,
+  //   fontWeight: '600',
+  // },
+  // sectionDescription: {
+  //   marginTop: 8,
+  //   fontSize: 18,
+  //   fontWeight: '400',
+  // },
+  // highlight: {
+  //   fontWeight: '700',
+  // },
+});
+
+export default App;
---
+++ src/Screens/Login/index.js
@@ -0,0 +1,139 @@
+import React, {Component} from 'react';
+import {
+  Text,
+  SafeAreaView,
+  View,
+  Image,
+  StyleSheet,
+  // TextInput,
+  Button,
+  ScrollView,
+} from 'react-native';
+
+import {TextInput as MetarialTextInput} from 'react-native-paper';
+
+export default class index extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {emailError: false, emailValue: '',passwordError: false, passwordValue: ''};
+  }
+  loginButtonHandler = () => {
+    console.log("clicked")
+    if (this.state.emailValue=='') {
+      console.log("email is null")
+      this.setState({
+        emailError:true,
+       
+      })
+      
+    } else {
+      console.log('else')
+      this.setState({
+        emailError:false,
+
+      })
+    }
+
+    if (this.state.passwordValue=='') {
+      this.setState({
+        passwordError:true,
+      })
+    }
+      else {
+        this.setState({
+          passwordError:false,
+        })
+      }
+  
+  
+  }
+  emailValueChanged = (text)=>{
+    console.log("email vaule is ......",text)
+    this.setState({
+      emailValue:text,
+    })
+  }
+  passwordValueChanged = (textP)=>{
+    console.log("password vaule is ......",textP)
+    this.setState({
+      passwordValue:textP,
+    })
+  }
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <ScrollView>
+          <View style={{flex: 1}}>
+            <View
+              style={{
+                justifyContent: 'center',
+                alignItems: 'center',
+                marginTop: '5%',
+              }}>
+              <Image
+                style={{height: 200, width: 200}}
+                source={require('../../Images/logo.png')}
+              />
+              <Text
+                style={{
+                  color: '#932324',
+                  fontSize: 24,
+                  marginTop: '5%',
+                  fontWeight: '500',
+                }}>
+                Login system
+              </Text>
+            </View>
+            <View
+              style={{marginTop: '5%', marginRight: '5%', marginLeft: '5%'}}>
+              <MetarialTextInput
+                style={{marginTop: '5%'}}
+                label="Email"
+                mode="outlined"
+                placeholder="Enter Your name"
+                error={this.state.emailError}
+                value={this.state.emailValue}
+                onChangeText={this.emailValueChanged}
+              />
+              <MetarialTextInput
+                label="Password"
+                mode="outlined"
+                secureTextEntry
+                // right={<MetarialTextInput.Icon name="eye" />}
+                style={{marginTop: '5%', marginBottom: '5%'}}
+                placeholder="password"
+
+                error={this.state.passwordError}
+                value={this.state.passwordValue}
+                onChangeText={this.passwordValueChanged}
+
+              />
+
+              <Button
+                icon="camera"
+                title="Login"
+                mode="contained"
+                color="#841584"
+                onPress={this.loginButtonHandler}
+              />
+            </View>
+          </View>
+        </ScrollView>
+      </SafeAreaView>
+    );
+  }
+}
+const styles = StyleSheet.create({
+  input: {
+    // height: 40,
+    // // margin: 12,
+    // borderWidth: 1,
+    // padding: 10,
+    // backgroundColor: '#dfdfdf',
+    // borderRadius: 5,
+    // borderColor: '#fef',
+    // justifyContent: 'center',
+    // alignItems: 'center',
+    // width: '100%',
+  },
+});
