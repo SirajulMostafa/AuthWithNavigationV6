ef1cbbd6a5aeab0e3b33ef2a8b85a02250ccf8c7
sirajul Mostafa <sirajulmost@gmail.com>
2021-11-08 21:29:44 +0600

    call API

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,15 +1,20 @@
 import React, {Component} from 'react';
 import {Text, StyleSheet, View, FlatList,TouchableOpacity} from 'react-native';
 // import { Item } from 'react-native-paper/lib/typescript/components/List/List';
-
+import { Avatar } from 'react-native-paper';
 export default class index extends Component {
   constructor(props) {
     super(props);
+    this.state ={userList:[]}
     // itemsArray =['IOS','Android','React-native']
-    itemsArray = [
+    DATA = [
       {
         id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
         title: 'First Item',
+      },
+      {
+        id: 'bd7acbea-e1b1-46c2-aed5-3ad53abb28ba',
+        title: 'First Item 2',
       },
       {
         id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
@@ -20,32 +25,49 @@ export default class index extends Component {
         title: 'Third Item',
       },
     ];
-    //  DATA = [
-    //     {
-    //       id: "bd7acbea-c1b1-46c2-aed5-3ad53abb28ba",
-    //       title: "First Item",
-    //     },
-    //     {
-    //       id: "3ac68afc-c605-48d3-a4f8-fbd91aa97f63",
-    //       title: "Second Item",
-    //     },
-    //     {
-    //       id: "58694a0f-3da1-471f-bd96-145571e29d72",
-    //       title: "Third Item",
-    //     },
-    //   ];
-    // DATA=['IOS','abc','cde','efg']
+   
   }
   renderItem = rowItem => {
     // console.log('Test row item', rowItem);
 
     return (
       <View style={styles.item}>
-        <Text style={{fontSize: 24}}>{rowItem.item.title}</Text>
+        <View style={{flexDirection: 'row',}}>
+
+       
+        <Avatar.Image size={100} source={require('../../Images/logo.png')} />
+        <View style={{alignSelf: 'center',marginLeft: '5%',}}>
+        <Text style={{fontSize: 24}}>{rowItem.item.name}</Text>
+        <Text>{rowItem.item.email}</Text>
         <Text>{rowItem.item.id}</Text>
-      </View>
+        </View>
+         </View>
+         </View>
     );
   };
+  componentDidMount (){
+   fetch('https://jsonplaceholder.typicode.com/users')
+  .then(response => response.json())
+  .then(json =>{
+     console.log(json)
+    this.setState({
+      userList:json
+    })
+  } )
+
+      }
+
+//GetUsersList=()=>{
+  // return console.log('hello console 1')
+  // fetch('https://jsonplaceholder.typicode.com/users')
+  // .then((response) => response.json())
+  // .then((responsJson) =>
+  // { return console.log("Response is...",responsJson)}) 
+  //  .catch((error)=>{
+  //   console.error(error);
+  // });
+
+// }
 
   render() {
     return (
@@ -53,16 +75,16 @@ export default class index extends Component {
         <FlatList
           //    data={DATA}
 
-          data={itemsArray}
+          // data={DATA}
+          data={this.state.userList}
         renderItem={this.renderItem}
-          keyExtractor={rowItem => rowItem.id}
+          keyExtractor={(rowItem) => rowItem.id}
           // extraData={selectedId}  
         />
       </View>
     );
   }
 }
-
 const styles = StyleSheet.create({
   item: {
     padding: 20,
