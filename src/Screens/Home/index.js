acb6b27e11d3c31f0143195d663e2458d9da9e51
sirajul Mostafa <sirajulmost@gmail.com>
2021-11-13 20:32:09 +0600

    convert class to HOOK and call API

--- src/Screens/Home/index.js
+++ src/Screens/Home/index.js
@@ -1,101 +1,82 @@
-import React, {Component} from 'react';
-import {Text, StyleSheet, View, FlatList,TouchableOpacity} from 'react-native';
-// import { Item } from 'react-native-paper/lib/typescript/components/List/List';
-import { Avatar } from 'react-native-paper';
-export default class index extends Component {
-  constructor(props) {
-    super(props);
-    this.state ={userList:[]}
-    // itemsArray =['IOS','Android','React-native']
-    DATA = [
-      {
-        id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item',
-      },
-      {
-        id: 'bd7acbea-e1b1-46c2-aed5-3ad53abb28ba',
-        title: 'First Item 2',
-      },
-      {
-        id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-        title: 'Second Item',
-      },
-      {
-        id: '58694a0f-3da1-471f-bd96-145571e29d72',
-        title: 'Third Item',
-      },
-    ];
-   
-  }
-  renderItem = rowItem => {
+import React, {useState, useEffect} from 'react';
+import {Text, StyleSheet, View, FlatList, TouchableOpacity} from 'react-native';
+import {ActivityIndicator, Avatar} from 'react-native-paper';
+import index from '../Login';
+const Index = () => {
+
+  const [userList, setUserList] = useState([]);
+  const [currentPage, setCurrentPage] = useState([])
+  
+  const renderItem = rowItem => {
     // console.log('Test row item', rowItem);
 
     return (
       <View style={styles.item}>
-        <View style={{flexDirection: 'row',}}>
-
-       
-        <Avatar.Image size={100} source={require('../../Images/logo.png')} />
-        <View style={{alignSelf: 'center',marginLeft: '5%',}}>
-        <Text style={{fontSize: 24}}>{rowItem.item.name}</Text>
-        <Text>{rowItem.item.email}</Text>
-        <Text>{rowItem.item.id}</Text>
+        <View style={{flexDirection: 'row'}}>
+          <Avatar.Image size={100} source={require('../../Images/logo.png')} />
+          <View style={{alignSelf: 'center'}}>
+            <Text style={{fontSize: 20}}>{rowItem.item.title}</Text>
+            <Text>{rowItem.item.body}</Text>
+            <Text>{rowItem.item.id}</Text>
+          </View>
         </View>
-         </View>
-         </View>
-    );
-  };
-  componentDidMount (){
-   fetch('https://jsonplaceholder.typicode.com/users')
-  .then(response => response.json())
-  .then(json =>{
-     console.log(json)
-    this.setState({
-      userList:json
-    })
-  } )
-
-      }
-
-//GetUsersList=()=>{
-  // return console.log('hello console 1')
-  // fetch('https://jsonplaceholder.typicode.com/users')
-  // .then((response) => response.json())
-  // .then((responsJson) =>
-  // { return console.log("Response is...",responsJson)}) 
-  //  .catch((error)=>{
-  //   console.error(error);
-  // });
-
-// }
-
-  render() {
-    return (
-      <View style={styles.container}>
-        <FlatList
-          //    data={DATA}
-
-          // data={DATA}
-          data={this.state.userList}
-        renderItem={this.renderItem}
-          keyExtractor={(rowItem) => rowItem.id}
-          // extraData={selectedId}  
-        />
       </View>
     );
-  }
+  };
+const renderLoader=()=>{
+  return(
+    <View style={styles.renderLoader}>
+      <ActivityIndicator size="large" color="#aaa"/>
+    </View>
+  )
 }
+
+  useEffect(() => {
+    getUsersList()
+  }, [])
+   const getUsersList=()=>{
+    fetch('https://jsonplaceholder.typicode.com/posts')
+    .then(response => response.json())
+    .then(responsJson => {
+      setUserList(responsJson)
+      return console.log('Response is...', responsJson);
+    })
+    .catch(error => {
+      console.error(error);
+    });
+}
+
+    
+
+  return (
+    <View style={styles.container}>
+      <FlatList
+        //    data={DATA}
+        data={userList}
+        renderItem={renderItem}
+        keyExtractor={rowItem => rowItem.id}
+        // extraData={selectedId}
+        ListFooterComponent={renderLoader}
+      />
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   item: {
-    padding: 20,
+    // padding: 20,
     marginVertical: 8,
-    marginHorizontal: 16,
+    // marginHorizontal: 16,
     borderBottomColor: '#999',
-    borderBottomWidth: 2,
+    // borderBottomWidth: 2,
     backgroundColor: '#f9c2ff',
   },
   container: {
     flex: 1,
     // marginTop: StatusBar.currentHeight || 0,
   },
+  renderLoader:{
+    marginHorizontal: 10,
+  }
 });
+export default Index;
