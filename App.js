a2c6b5b4d61db8fb451e7818b119fd70a07c0962
sirajul Mostafa <sirajulmost@gmail.com>
2021-12-05 14:50:45 +0600

    ReArange code for auth

--- App.js
+++ App.js
@@ -1,123 +1,21 @@
 import React from 'react';
-import {StyleSheet,ImageBackground,Image} from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-import LoginScreen from './//src/Screens/Login';
-import HomeScreen from './/src/Screens/Home'
-import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import { createDrawerNavigator } from '@react-navigation/drawer';
-import ProfileDetails from './src/Screens/Home/ProfileDetails';
-
-import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import {StyleSheet, ImageBackground, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { TabModule } from './src/Routing';
 const App = () => {
-  // return <LoginScreen />;
-const Stack= createNativeStackNavigator();
-const Tab = createMaterialBottomTabNavigator();
-const Drawer=createDrawerNavigator();
-
- const HomeStack =()=>{
-   return(
-    <Stack.Navigator>
-    <Stack.Screen name="HomeStack" 
-    component={HomeScreen}
-    options={{
-      title:'Home'
-    }} 
-     />
-    <Stack.Screen name="ProfileDetails"
-     component={ProfileDetails}
-     options={{
-      title:'Profile'
-    }} 
-     />
-  </Stack.Navigator>
-   )
- }
-
- const DrawerScreen = ()=>{
-   return(
-    <Drawer.Navigator>
-    <Drawer.Screen name="Home" component={HomeScreen} />
-    <Drawer.Screen name="profile" component={ProfileDetails} />
-  </Drawer.Navigator>
-   )
- }
-
   return (
-
-  
-    // <ImageBackground source={image} resizeMode="cover" style={styles.image}>
-         
     <NavigationContainer>
-    <Tab.Navigator
-     initialRouteName="Home"
-     activeColor="orange"
-     inactiveColor="gray"
-     
-     barStyle={{ backgroundColor: 'red' }}
-    // barStyle={{ paddingBottom: 48 }}
-    >
-     <Tab.Screen name="Home"   
-      component={HomeStack}
-      options={{
-        tabBarLabel: 'Home',
-        tabBarBadge:10,
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="home" color={color} size={26} />
-        ),
-
-      }}
-      />
-      <Tab.Screen name="Login"
-      component={LoginScreen} 
-       options={{
-        tabBarLabel: 'login',
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="account" color={color} size={26} />
-        ),
-      }}
-       />
-       <Tab.Screen name="LoginScreen"
-      component={DrawerScreen} 
-       options={{
-        tabBarLabel: 'drawer',
-        tabBarIcon: ({ color }) => (
-          <MaterialCommunityIcons name="account" color={color} size={26} />
-        ),
-      }}
-       />
-    </Tab.Navigator>
-  </NavigationContainer>
-  
-        //  {/* <HomeScreen/> */}
-        //  </ImageBackground>
-  )
-  
-
+      <TabModule />
+    </NavigationContainer>
+  );
 };
 
 const styles = StyleSheet.create({
-  
   image: {
     flex: 1,
-    justifyContent: "center"
+    justifyContent: 'center',
   },
-  // sectionContainer: {
-  //   marginTop: 32,
-  //   paddingHorizontal: 24,
-  // },
-  // sectionTitle: {
-  //   fontSize: 24,
-  //   fontWeight: '600',
-  // },
-  // sectionDescription: {
-  //   marginTop: 8,
-  //   fontSize: 18,
-  //   fontWeight: '400',
-  // },
-  // highlight: {
-  //   fontWeight: '700',
-  // },
 });
 
 export default App;
---
+++ src/Routing/index.js
@@ -0,0 +1,85 @@
+import * as React from 'react';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import LoginScreen from '../Screens/Login';
+import HomeScreen from '../Screens/Home';
+import ProfileDetails from '../Screens/Home/ProfileDetails';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {createMaterialBottomTabNavigator} from '@react-navigation/material-bottom-tabs';
+
+
+const Stack = createNativeStackNavigator();
+const Tab = createMaterialBottomTabNavigator();
+const Drawer = createDrawerNavigator();
+
+export const TabModule=()=>{
+    return(
+      <Tab.Navigator
+         initialRouteName="Home"
+         activeColor="orange"
+         inactiveColor="gray"
+         
+         barStyle={{ backgroundColor: 'red' }}
+        // barStyle={{ paddingBottom: 48 }}
+        >
+         <Tab.Screen name="Home"   
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarBadge:10,
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="home" color={color} size={26} />
+            ),
+    
+          }}
+          />
+          <Tab.Screen name="Login"
+          component={LoginScreen} 
+           options={{
+            tabBarLabel: 'login',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+          }}
+           />
+           <Tab.Screen name="LoginScreen"
+          component={DrawerScreen} 
+           options={{
+            tabBarLabel: 'drawer',
+            tabBarIcon: ({ color }) => (
+              <MaterialCommunityIcons name="account" color={color} size={26} />
+            ),
+          }}
+           />
+        </Tab.Navigator>
+    )
+    }
+    
+    
+     export const HomeStack =()=>{
+      return(
+       <Stack.Navigator>
+       <Stack.Screen name="HomeStack" 
+       component={HomeScreen}
+       options={{
+         title:'Home'
+       }} 
+        />
+       <Stack.Screen name="ProfileDetails"
+        component={ProfileDetails}
+        options={{
+         title:'Profile'
+       }} 
+        />
+     </Stack.Navigator>
+      )
+    }
+    
+     export const DrawerScreen = ()=>{
+      return(
+       <Drawer.Navigator>
+       <Drawer.Screen name="Home" component={HomeScreen} />
+       <Drawer.Screen name="profile" component={ProfileDetails} />
+     </Drawer.Navigator>
+      )
+    }
